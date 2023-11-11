5dc92eae7d9051b4fad706ff01c39ac6c0870667
Asiri Dewthilina <asiridewthilina@Asiris-Mac-mini.local>
2023-11-11 23:49:30 +0530

    sig change ad bio

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ const page = () => {
             link="https://t.me/RedMarkets"
           />
           <Banners
-            url="https://cdn.discordapp.com/attachments/1087681487408730144/1170978932187742279/DALLE_2023-11-06_10.57.31_-_Refine_the_logo_for_Stake_Bets_a_sports_betting_platform_with_a_modern_and_sleek_design_that_conveys_the_excitement_of_sports_betting._The_logo_sh_1_1.png?ex=655b021d&is=65488d1d&hm=f6df179688595120a114bf2747a8e5504e802411b436f2e685c73d74617f5164&"
+            url="https://i.imgur.com/sWHEbMR.jpg"
             link="https://t.me/stakebets"
           />
         </div>
