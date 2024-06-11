f572ce1c8cce130c3eef116a4be5a4a74702882b
Asiri Dewthilina <asiridewthilina@Asiris-Mac-mini.local>
2024-06-11 08:45:27 +0530

    db

--- components/TextArea.jsx
+++ components/TextArea.jsx
@@ -2,47 +2,43 @@
 import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 
-const backendUrl ='https://rich-pink-panda-wear.cyclic.app/api/text'
+const backendUrl = "https://pastesitebackend.onrender.com/api/text";
 
 const TextArea = () => {
   const [textFeild, setTextFeild] = useState("");
 
   const pasteText = async () => {
-
     try {
-        const response = await fetch(backendUrl , 
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                content: textFeild,
-              }),
-            }
-          );
+      const response = await fetch(backendUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: textFeild,
+        }),
+      });
 
-          if(response.ok) {
-            const result = await response.json();
-            const id = result._id;
-            redirectToPastedText(id)
-            console.log(result);
-            setTextFeild("");
-            toast.success("Please wait..")
-          }
-        
+      if (response.ok) {
+        const result = await response.json();
+        const id = result._id;
+        redirectToPastedText(id);
+        console.log(result);
+        setTextFeild("");
+        toast.success("Please wait..");
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
   };
-  
+
   const redirectToPastedText = (id) => {
     window.location.href = `/pasted/${id}`;
   };
-  
+
   return (
     <div className="">
-    <textarea
+      <textarea
         className="outline-none border  bg-transparent bg-opacity-20 backdrop-filter backdrop-blur-md  border-white shadow-md rounded placeholder:text-white placeholder:text-center text-white w-[100%] md:min-w-[50rem] sm:min-w-[40rem] min-w-[20rem] font-semibold placeholder:mt-4"
         placeholder="Paste your text here"
         rows={10}
@@ -52,7 +48,10 @@ const TextArea = () => {
       />
 
       <div>
-        <button className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={pasteText}>
+        <button
+          className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={pasteText}
+        >
           Paste
         </button>
       </div>
@@ -61,4 +60,3 @@ const TextArea = () => {
 };
 
 export default TextArea;
-
